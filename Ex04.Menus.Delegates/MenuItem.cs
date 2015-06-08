35b7b35f15b61e82221d14375c3aa9e260e9ff70
ThaliaDeShalit <thaliapenguin@gmail.com>
2015-06-08 16:52:47 +0300

    finished delegates

--- Ex04.Menus.Delegates/MenuItem.cs
+++ Ex04.Menus.Delegates/MenuItem.cs
@@ -7,5 +7,50 @@ namespace Ex04.Menus.Delegates
     class MenuItem
     {
         public event Action<MenuItem> clicked;
+
+        private List<MenuItem> m_SubItems;
+        private string m_Header;
+
+        public MenuItem(string i_Header)
+        {
+            m_SubItems = new List<MenuItem>();
+            m_Header = i_Header;
+        }
+
+        public List<MenuItem> SubItems
+        {
+            get
+            {
+                return m_SubItems;
+            }
+        }
+
+        public string Header
+        {
+            get
+            {
+                return m_Header;
+            }
+        }
+
+        internal bool IsSubMenu()
+        {
+            bool isSubMenu = true;
+
+            if (m_SubItems.Count == 0)
+            {
+                isSubMenu = false;
+            }
+
+            return isSubMenu;
+        }
+
+        public void onClicked()
+        {
+            if (clicked != null)
+            {
+                clicked(this);
+            }
+        }
     }
 }

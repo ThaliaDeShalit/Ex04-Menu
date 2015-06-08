cdc84cc2b7377318614a0e95447fde028ac3c08a
Nadav <nadavglickman@gmail.com>
2015-06-08 19:08:25 +0300

    deleted unused code in Program

--- Ex04.Menus.Test/Program.cs
+++ Ex04.Menus.Test/Program.cs
@@ -9,41 +9,8 @@ namespace Ex04.Menus.Test
     {
         public static void Main()
         {
-
-            //MenuItem file = new MenuItem("File");
-            //NewProjectFunction newProject = new NewProjectFunction("New Project");
-            //MenuItem add = new MenuItem("Add");
-            //add.SubItems.Add(newProject);
-
-            //file.SubItems.Add(newProject);
-            //file.SubItems.Add(add);
-
-            //MainMenu main = new MainMenu(file);
-            //main.Show();
-
             TestMenuInterface test = new TestMenuInterface();
             //TestMenuDelegate delegateTest = new TestMenuDelegate();
-
         }       
-
-        class NewProjectFunction : MenuItem, IClickable
-        {
-            public NewProjectFunction(String i_Header)
-                : base(i_Header)
-            {
-
-            }
-
-            void IClickable.DoSomething()
-            {
-                startNewProject();
-            }
-
-            public void startNewProject()
-            {
-                Console.WriteLine("~~~~~~ STARTING NEW PROJECT ~~~~~~");
-                Console.ReadLine();
-            }
-        }
     }
 }

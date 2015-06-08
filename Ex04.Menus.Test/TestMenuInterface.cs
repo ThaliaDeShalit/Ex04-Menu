b4f0f0972ef2657f7c59d8fb820f3eb381cd5688
ThaliaDeShalit <thaliapenguin@gmail.com>
2015-06-08 18:10:59 +0300

    Almost finished interface test

---
+++ Ex04.Menus.Test/TestMenuInterface.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ex04.Menus.Interfaces;
+
+namespace Ex04.Menus.Test
+{
+    class TestMenuInterface
+    {
+        public TestMenuInterface()
+        {
+            createMainMenu();
+        }
+
+        private void createMainMenu()
+        {
+            MenuItem mainMenu = new MenuItem("Main Menu");
+
+            MenuItem showDateTimeMenu = createShowDateTimeMenu();
+            MenuItem infoMenu = createInfoMenu();
+        }
+
+        private MenuItem createShowDateTimeMenu()
+        {
+            MenuItem showDateTimeMenu = new MenuItem("Show Date/Time");
+            
+            ShowTime showTime = new ShowTime();
+            ShowDate showDate = new ShowDate();
+
+            showDateTimeMenu.SubItems.Add(showTime);
+            showDateTimeMenu.SubItems.Add(showDate);
+
+            return showDateTimeMenu;
+        }
+
+        private MenuItem createInfoMenu()
+        {
+            MenuItem infoMenu = new MenuItem("Info");
+
+            ShowVersion showVersion = new ShowVersion();
+            CountWords countWords = new CountWords();
+
+            infoMenu.SubItems.Add(showVersion);
+            infoMenu.SubItems.Add(countWords);
+
+            return infoMenu;
+        }
+    }
+}
